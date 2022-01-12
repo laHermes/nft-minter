6376b74f6799d052440a1fe31b7c30af3dae0c7b
Lazar Jankovic <jlazar997@gmail.com>
2022-01-12 11:45:37 +0100

    write server & config tailwind

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,47 @@
-import * as functions from 'firebase-functions';
-import * as firebase from 'firebase-admin';
-import credentials from './wallet-auth.json';
+import dotenv from 'dotenv';
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
 
-const serviceAccount = credentials as firebase.ServiceAccount;
+dotenv.config();
 
-firebase.initializeApp({
-	credential: firebase.credential.cert(serviceAccount),
-});
+const DB_PASSWORD = process.env.DB_PASSWORD || '';
+const DB_USERNAME = process.env.DB_USERNAME || '';
 
-const db = firebase.firestore();
+if (DB_PASSWORD === '') {
+	console.warn('Must provide DB_PASSWORD environment variable');
+	process.exit(1);
+}
 
-export const generateNonce = () => {};
+if (DB_USERNAME === '') {
+	console.warn('Must provide DB_USERNAME environment variable');
+	process.exit(1);
+}
 
-export const verifyMessage = () => {};
+const SERVER_PORT = process.env.SERVER_PORT || 3001;
+
+const app = express();
+
+// start server function
+const startServer = async () => {
+	const httpServer = http.createServer(app);
+
+	// Mongo DB connection
+	mongoose
+		.connect(
+			`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.lplof.gcp.mongodb.net/r_p_s?retryWrites=true&w=majority`
+		)
+		.then(() => console.log('Connected to MongoDB!'))
+		.catch((err) => console.warn('Error in MongoDB connection!', err));
+
+	httpServer.listen(SERVER_PORT, () => {
+		console.log(`======================================================`);
+		console.log(`HTTP Server ready at  http://localhost:${SERVER_PORT}`);
+		console.log(
+			`Apollo Server ready at  http://localhost:${SERVER_PORT}${server.graphqlPath}`
+		);
+		console.log(`======================================================`);
+	});
+};
+
+startServer();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [],
+	darkMode: 'class', // or 'media' or 'class'
+	theme: {
+		container: {
+			center: true,
+			screens: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
+			},
+		},
+		fontSize: {
+			xxs: '.5rem',
+			xs: '.75rem',
+			sm: '.875rem',
+			tiny: '.875rem',
+			base: '1rem',
+			lg: '1.125rem',
+			xl: '1.25rem',
+			'2xl': '1.5rem',
+			'3xl': '1.875rem',
+			'4xl': '2.25rem',
+			'5xl': '3rem',
+			'6xl': '4rem',
+			'7xl': '5rem',
+		},
+		extend: {},
+	},
+	plugins: [],
+};
