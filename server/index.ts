ab050c9c252a98aa17e644affd28104ac9969bb0
Lazar Jankovic <jlazar997@gmail.com>
2022-01-12 11:13:27 +0100

    add ethers tailwind react-router-dom

--- server/index.ts
+++ server/index.ts
@@ -2,8 +2,14 @@ import * as functions from 'firebase-functions';
 import * as firebase from 'firebase-admin';
 import credentials from './wallet-auth.json';
 
+const serviceAccount = credentials as firebase.ServiceAccount;
+
 firebase.initializeApp({
-	credential: firebase.credential.cert(credentials),
+	credential: firebase.credential.cert(serviceAccount),
 });
 
 const db = firebase.firestore();
+
+export const generateNonce = () => {};
+
+export const verifyMessage = () => {};
