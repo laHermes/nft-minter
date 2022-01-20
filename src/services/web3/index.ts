0cfd0a57c66758d3d0ce7016532879b0b3df9eed
Lazar Jankovic <jlazar997@gmail.com>
2022-01-20 15:31:03 +0100

    wallet handling hook

--- src/services/web3/index.ts
+++ src/services/web3/index.ts
@@ -4,11 +4,31 @@ import { providers } from 'ethers';
 import { EthNetworks } from './types';
 import { createUrl } from './wallet/connectors';
 
+export type TProviders = providers.BaseProvider | providers.WebSocketProvider;
+
 export const getProvider = (
 	network: EthNetworks = EthNetworks.Mumbai
-): providers.BaseProvider | providers.WebSocketProvider => {
+): TProviders => {
 	if (process.env.REACT_APP_MUMBAI) {
 		return new providers.WebSocketProvider(createUrl(network, true));
 	}
 	return new providers.BaseProvider(createUrl(network));
+};
+
+export const writeWeb3 = {
+	signer: (window as any).ethereum
+		? new Web3Provider((window as any).ethereum).getSigner()
+		: new providers.WebSocketProvider(
+				createUrl(EthNetworks.Mumbai, true)
+		  ).getSigner(),
+};
+
+export const setProvider = (provider: TProviders) => {
+	web3.provider = provider;
+};
+
+export const setSigner = (signer: providers.JsonRpcSigner) => {};
+
+export const web3 = {
+	provider: getProvider(),
 };
--- src/services/web3/wallet/useWalletconnect.ts
+++ src/services/web3/wallet/useWalletconnect.ts
@@ -1,7 +1,34 @@
 import { useCallback, useMemo, useState } from 'react';
+import { useWeb3React } from '@web3-react/core';
+import { setAutoLoginLS } from '../../../utils/localStorage';
 
 const useWalletconnect = () => {
-	return true;
+	const { activate, deactivate, account, connector } = useWeb3React();
+
+	const [isPending, setIsPending] = useState<boolean>(false);
+	const [isError, setIsError] = useState<boolean>(false);
+
+	const handleOpen = useCallback(() => {
+		setIsError(false);
+		setIsPending(true);
+	}, []);
+
+	const handleConnect = useCallback(async () => {
+		connector && (await activate(connector, undefined, true));
+		setAutoLoginLS(true);
+	}, [activate, connector]);
+
+	const handleDisconnect = useCallback(() => {
+		deactivate();
+
+		setAutoLoginLS(false);
+	}, [deactivate]);
+
+	const handleWalletConnectButton = useCallback(() => {
+		account ? handleDisconnect() : handleConnect();
+	}, [account, handleDisconnect, handleConnect]);
+
+	return { isError, isPending, handleOpen, account, handleWalletConnectButton };
 };
 
 export default useWalletconnect;
