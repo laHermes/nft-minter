03efdd70a8ccc21c3eb4e71378cb7a5b8ffb755a
Lazar Jankovic <jlazar997@gmail.com>
2022-02-05 16:21:01 +0100

    multicall get owners

--- src/services/web3/multicall/multicall.ts
+++ src/services/web3/multicall/multicall.ts
@@ -2,13 +2,12 @@ import { Contract, Provider } from 'ethcall';
 import { getProvider } from '..';
 import { store } from '../../../redux';
 import Collection from '../../../contracts/Collection.json';
-import { BigNumber, BigNumberish, utils } from 'ethers';
-import { isBigNumberish } from '@ethersproject/bignumber/lib/bignumber';
+import { BigNumberish, utils } from 'ethers';
 
-type ethData = BigNumber | boolean;
-
-const NFT_ADDRESS = '0xf15bF72Da22db469F5375A9f55c88000a159b8dE';
+const NFT_ADDRESS = '0xd1B1193A591139CB46f3B55a341a92F4C6791B31';
 const provider = store.dispatch(getProvider);
+
+type OwnersType = null | string;
 
 export const mulitCall = async () => {
 	const ethcallProvider = new Provider();
@@ -19,22 +18,35 @@ export const mulitCall = async () => {
 	const tokenPriceReq = contract.TOKEN_PRICE();
 	const totalSupplyReq = contract.totalSupply();
 	const saleStatusReq = contract.openSale();
+	const nameReq = contract.name();
 
 	const data: BigNumberish[] = await ethcallProvider.all([
 		maxTokensReq,
 		tokenPriceReq,
 		totalSupplyReq,
 		saleStatusReq,
+		nameReq,
 	]);
 
-	console.log(dataFormat(data));
+	const preData = dataFormat(data);
+
+	const tokenOwners = [];
+	console.log(preData);
+
+	for (let i = 0; i < preData.maxMintableTokens; i++) {
+		tokenOwners.push(contract.ownerOf(i));
+	}
+
+	const dataSec: OwnersType[] = await ethcallProvider.tryAll(tokenOwners);
+	console.log(dataSec);
 };
 
-const dataFormat = (data: BigNumberish[]) => {
+const dataFormat = (data: BigNumberish[], granularity: number = 0) => {
 	return {
 		tokenPrice: utils.formatEther(data[1]),
-		maxMintableTokens: utils.formatUnits(data[0], 0),
-		totalSupply: utils.formatUnits(data[2], 0),
+		maxMintableTokens: parseInt(utils.formatUnits(data[0], granularity)),
+		totalSupply: utils.formatUnits(data[2], granularity),
 		saleStatus: data[3],
+		nftName: data[4],
 	};
 };
