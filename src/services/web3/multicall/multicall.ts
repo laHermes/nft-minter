41453214d1b03738209349691a810426434a758c
Lazar Jankovic <jlazar997@gmail.com>
2022-02-05 16:28:13 +0100

    multicall cleanup

--- src/services/web3/multicall/multicall.ts
+++ src/services/web3/multicall/multicall.ts
@@ -4,10 +4,18 @@ import { store } from '../../../redux';
 import Collection from '../../../contracts/Collection.json';
 import { BigNumberish, utils } from 'ethers';
 
+type OwnersType = null | string;
+
+interface IDataFormat {
+	tokenPrice: string;
+	maxMintableTokens: number;
+	totalSupply: string;
+	saleStatus: boolean;
+	nftName: string;
+}
+
 const NFT_ADDRESS = '0xd1B1193A591139CB46f3B55a341a92F4C6791B31';
 const provider = store.dispatch(getProvider);
-
-type OwnersType = null | string;
 
 export const mulitCall = async () => {
 	const ethcallProvider = new Provider();
@@ -20,6 +28,7 @@ export const mulitCall = async () => {
 	const saleStatusReq = contract.openSale();
 	const nameReq = contract.name();
 
+	// first multicall
 	const data: BigNumberish[] = await ethcallProvider.all([
 		maxTokensReq,
 		tokenPriceReq,
@@ -28,25 +37,32 @@ export const mulitCall = async () => {
 		nameReq,
 	]);
 
+	//returns formated data
 	const preData = dataFormat(data);
 
 	const tokenOwners = [];
-	console.log(preData);
 
+	// iterate max mintable tokens to fetch owner
 	for (let i = 0; i < preData.maxMintableTokens; i++) {
 		tokenOwners.push(contract.ownerOf(i));
 	}
 
+	// second multicall
+	//returns either owner's address or null
 	const dataSec: OwnersType[] = await ethcallProvider.tryAll(tokenOwners);
+
 	console.log(dataSec);
 };
 
-const dataFormat = (data: BigNumberish[], granularity: number = 0) => {
+const dataFormat = (
+	data: BigNumberish[],
+	granularity: number = 0
+): IDataFormat => {
 	return {
 		tokenPrice: utils.formatEther(data[1]),
 		maxMintableTokens: parseInt(utils.formatUnits(data[0], granularity)),
 		totalSupply: utils.formatUnits(data[2], granularity),
-		saleStatus: data[3],
-		nftName: data[4],
+		saleStatus: !!data[3],
+		nftName: data[4].toString(),
 	};
 };
