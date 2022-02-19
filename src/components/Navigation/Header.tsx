d9168ef9ec4b4a0948c105647a107cb658dba71c
Lazar Jankovic <jlazar997@gmail.com>
2022-02-19 14:20:41 +0100

    add gradient bg

--- src/components/Navigation/Header.tsx
+++ src/components/Navigation/Header.tsx
@@ -31,23 +31,32 @@ const sideComponents: IComponent[] = [
 //HEADER COMPONENT FOR NAVIGATION
 const Header = () => {
 	return (
-		<header className='bg-gray-800 shadow py-2 px-4'>
+		<header className='backdrop-blur-sm bg-gray-800/50 shadow py-2 px-4'>
 			<div className='flex items-center flex-row justify-between'>
 				<div className='inline-flex'>
 					<span className='text-indigo-100 text-2xl font-black inter'>
 						nft.
 					</span>
 				</div>
-				<Link to='/mint'>
-					<button className='px-2'>
-						<div className='inline-flex'>
-							<p className='text-md'>MINTER</p>
-						</div>
-					</button>
-				</Link>
-				<p>Collection</p>
-				<p>Yield Farming</p>
-				<div>
+				<div className='flex flex-row text-lg text-indigo-50'>
+					<Link to='/mint'>
+						<button className='px-2'>
+							<p className='inline-flex font-bold border p-1'>Minter</p>
+						</button>
+					</Link>
+					<Link to='/mint'>
+						<button className='px-2'>
+							<p className='inline-flex font-bold border p-1'>Collection</p>
+						</button>
+					</Link>
+					<Link to='/mint'>
+						<button className='px-2'>
+							<p className='inline-flex font-bold border p-1'>Yield Farming</p>
+						</button>
+					</Link>
+				</div>
+
+				{/* <div>
 					{sideComponents.map((component: any) => {
 						//maps component from array of component
 						//renders dynamic component from array
@@ -65,12 +74,9 @@ const Header = () => {
 							</Link>
 						);
 					})}
-				</div>
+				</div> */}
 
 				<div className='flex flex-row gap-2 text-xl'>
-					<div className='bg-slate-900 font-semibold text-indigo-50 px-2 pv-1 rounded-md'>
-						Dark
-					</div>
 					<div className='bg-slate-900 font-semibold text-indigo-50 px-4 pv-1 rounded-md '>
 						Polygon Mumbai
 					</div>
--- src/components/views/Mint/Mint.tsx
+++ src/components/views/Mint/Mint.tsx
@@ -3,14 +3,32 @@ import React from 'react';
 const Mint = () => {
 	return (
 		<div className='flex flex-col justify-between  max-w-screen-sm mx-auto'>
-			{/* <p>
-				For minting 1 NFt you will receive 1000 LTK tokens which you can stake
-			</p> */}
-			<div className='bg-zinc-900 text-indigo-100 rounded-3xl mt-12'>
-				{/* <p>Price 0.05 mMATIC</p> */}
+			<div className='bg-zinc-900/95 text-indigo-100 rounded-3xl mt-12 shadow-sm'>
 				<div className='flex flex-col gap-3 p-3'>
-					<div className='w-full p-3  rounded-xl bg-teal-900/40 text-teal-300/70 font-black'>
+					<div className='w-full p-3 rounded-xl bg-teal-900/40 text-teal-300/70 font-black'>
 						<p> Mint 1 NFT + get 1000 LTK tokens</p>
+					</div>
+					<div className='w-full p-3 rounded-xl bg-teal-900/20 text-emerald-300/60 font-black text-center'>
+						<p> PRICE 0.05 mMATIC</p>
+					</div>
+
+					<div className='bg-zinc-800 px-3 py-2 flex flex-row justify-between gap-2 rounded-xl'>
+						<input
+							type='text'
+							className='bg-inherit text-indigo-50 text-2xl font-bold rounded-xl flex-0'
+							value={0}
+						/>
+						<p className='bg-zinc-700/75 px-3 py-2 rounded-xl font-bold'>
+							NFT(s)
+						</p>
+					</div>
+					<div className='bg-zinc-800 p-2 flex flex-row gap-2 rounded-xl'>
+						<p className='bg-inherit text-indigo-50 text-2xl font-bold rounded-xl flex-1'>
+							0
+						</p>
+						<p className='bg-zinc-700/75 px-3 py-2 rounded-xl font-bold'>
+							LTK tokens
+						</p>
 					</div>
 					<div className='flex flex-row justify-between gap-2 '>
 						<button className='bg-zinc-800 text-indigo-50 leading-none text-4xl font-black px-3 py-1 rounded-xl flex-1'>
@@ -20,30 +38,12 @@ const Mint = () => {
 							-
 						</button>
 					</div>
-					<div className='bg-zinc-800 px-3 py-2 flex flex-row justify-between gap-2 rounded-xl'>
-						<input
-							type='text'
-							className=' bg-inherit text-indigo-50 text-2xl font-bold rounded-xl flex-0'
-							value={0}
-						/>
-						<p className='bg-zinc-700/75 px-3 py-2 rounded-xl font-bold'>
-							NFT(s)
-						</p>
-					</div>
-					<div className='bg-zinc-800 p-2 flex flex-row gap-2 rounded-xl'>
-						<p className=' bg-inherit text-indigo-50 text-2xl font-bold rounded-xl flex-1'>
-							0
-						</p>
-						<p className='bg-zinc-700/75 px-3 py-2 rounded-xl font-bold'>
-							LTK tokens
-						</p>
-					</div>
 					<button className='bg-sky-900/40 text-sky-600/70 py-4 font-black w-full rounded-3xl text-lg'>
 						Mint
 					</button>
 				</div>
 			</div>
-			<div>
+			<div className='flex flex-row p-3 text-indigo-200 font-bold gap-2'>
 				<p>OPENSEA</p>
 				<p>PolyScan</p>
 			</div>
